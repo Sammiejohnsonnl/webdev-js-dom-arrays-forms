b7aa9ebf2b632d0302c8d55f334f537f6a819c29
Okunowo <c3055721@hallam.shu.ac.uk>
2023-11-10 10:35:32 +0000

    Change current page color

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@
 (function(){
 //code starts here
 document.getElementById("myHeading").innerHTML = "Samuel";
-
+document.querySelector("nav ul li").setAttribute("class", "currentPage");
 // code ends here
 })();
