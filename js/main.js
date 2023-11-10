868a8e3cfb96a6d98c304da602975499c25daaa4
Okunowo <c3055721@hallam.shu.ac.uk>
2023-11-10 10:40:39 +0000

    example

--- js/main.js
+++ js/main.js
@@ -3,5 +3,7 @@
 //code starts here
 document.getElementById("myHeading").innerHTML = "Samuel";
 document.querySelector("nav ul li").setAttribute("class", "currentPage");
+
+document.querySelector('nav ul li a').setAttribute('href', 'http://www.google.co.uk');
 // code ends here
 })();
